657777ef2040641d1451302c7b2fa1f6276d6f3d
Apurv <31apurv@gmail.com>
2023-03-18 10:10:23 +0530

    used mongodb atlas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 app.set('view engine', 'ejs');
 //
-app.listen(3000,()=>console.log("Server started on port 3000"));
+app.listen(process.env.PORT||3000,()=>console.log("Server started on port 3000"));
 //
  mongoose.connect("mongodb+srv://Apurv:YukZV8WPzzJCS1G4@cluster0.hl6wk8j.mongodb.net/MealDB");
  const mealSchema = mongoose.Schema({
@@ -32,8 +32,8 @@ const meal1 = new meal({
      },
      date: new Date(2023,2,18).toLocaleString().split(',')[0]
 })
-meal1.save();
-meal2.save();
+// meal1.save();
+// meal2.save();
 app.get('/',(req,res)=>{
    const today = new Date().toLocaleString().split(',')[0];
    meal.find({date:today}).then((x)=>{
