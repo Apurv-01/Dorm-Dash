cbbe4af30dbee457e84d6bc2bbae986299321564
Apurv <31apurv@gmail.com>
2023-03-20 08:33:59 +0530

    added feature that allows user to see tomorrow's menu

--- app.js
+++ app.js
@@ -20,18 +20,31 @@ const meal = mongoose.model('meal',mealSchema);
 app.get('/',(req,res)=>{
   res.render("landing");
 })
+
 app.get('/mess-menu',(req,res)=>{
-   const today = new Date();
-   const dd = today.getDate();
-   const mm = today.getMonth()+1;
-   const yyyy = today.getFullYear();
-   const formattedToday = `${dd}/${mm}/${yyyy}`;
+   let today = new Date();
+   let dd = today.getDate();
+   let mm = today.getMonth()+1;
+   let yyyy = today.getFullYear();
+   let formattedToday = `${dd}/${mm}/${yyyy}`;
    console.log(today);
    meal.find({date:formattedToday}).then((x)=>{
       console.log(x);
       console.log(x[0]);
-    res.render("menu",{breakfast:x[0].name.breakfast,lunch:x[0].name.lunch,snacks:x[0].name.snacks,dinner:x[0].name.dinner});
+    res.render("menu",{Day:"Today",breakfast:x[0].name.breakfast,lunch:x[0].name.lunch,snacks:x[0].name.snacks,dinner:x[0].name.dinner});
    }).catch((err)=>console.log(err))
+})
+app.get('/tomorrow-mess-menu',(req,res)=>{
+  let tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);;
+  let tomorrowdd = tomorrow.getDate();
+  let tomorrowmm = tomorrow.getMonth()+1;
+  let tomorrowyyyy = tomorrow.getFullYear();
+  let formattedtomorrow = `${tomorrowdd}/${tomorrowmm}/${tomorrowyyyy}`;
+  meal.find({date:formattedtomorrow}).then((x)=>{
+     console.log(x);
+     console.log(x[0]);
+   res.render("menu",{Day:"Tomorrow",breakfast:x[0].name.breakfast,lunch:x[0].name.lunch,snacks:x[0].name.snacks,dinner:x[0].name.dinner});
+  }).catch((err)=>console.log(err))
 })
 app.listen(process.env.PORT||3000,()=>console.log(`Server started on port ${process.env.PORT}`));
 
